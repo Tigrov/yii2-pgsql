0327c99f656ea1bba7dfaadbc8d5e040e9db1e9d
Supply.Parts <www.supply.parts@gmail.com>
2017-05-03 17:12:27 +0300

    travis settings

--- tests/data/config.local.php
+++ tests/data/config.local.php
@@ -1,16 +1,5 @@
 <?php
 
 return [
-    'components' => [
-        'db' => [
-            'class' => 'yii\db\Connection',
-            'dsn' => 'pgsql:host=localhost;port=5434;dbname=country',
-            'username' => 'postgres',
-            'password' => 'YfGfktdt68',
-            'charset' => 'utf8',
-            'schemaMap' => [
-                'pgsql'=> 'tigrov\pgsql\Schema',
-            ],
-        ],
-    ],
+
 ];
