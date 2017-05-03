11c1d841cf5f14502fe3b54b0c439b3d4c8e18b8
Supply.Parts <www.supply.parts@gmail.com>
2017-05-03 17:34:21 +0300

    compatibility with PHPUnit < 6.x

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -13,3 +13,5 @@ require_once(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
 Yii::setAlias('@tigrov/tests/unit/pgsql', __DIR__);
 Yii::setAlias('@tigrov/pgsql', dirname(__DIR__));
+
+require_once(__DIR__ . '/compatibility.php');
