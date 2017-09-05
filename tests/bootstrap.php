6c7259dada9cef1f3a172c70185f148e6675c5fd
Tigrov <rrr-r@ya.ru>
2017-09-05 22:05:29 +0300

    fix test's bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,6 +5,7 @@ error_reporting(-1);
 
 define('YII_ENABLE_ERROR_HANDLER', false);
 define('YII_DEBUG', true);
+define('YII_ENV', 'test');
 $_SERVER['SCRIPT_NAME'] = '/' . __DIR__;
 $_SERVER['SCRIPT_FILENAME'] = __FILE__;
 
@@ -15,3 +16,4 @@ Yii::setAlias('@tigrov/tests/unit/pgsql', __DIR__);
 Yii::setAlias('@tigrov/pgsql', dirname(__DIR__));
 
 require_once(__DIR__ . '/compatibility.php');
+require_once __DIR__ . '/TestCase.php';
